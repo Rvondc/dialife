66135178174c51e591ebdf6ce4394c4fc567929c
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2023-10-25 16:12:30 +0800

    refactored to use margin

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:dialife/bmi_tracking/input_form.dart';
 import 'package:dialife/bmi_tracking/record_editor.dart';
 import 'package:dialife/insulin_tracking/insulin_tracking.dart';
 import 'package:dialife/setup.dart';
+import 'package:dialife/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -45,7 +46,6 @@ class Main extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       onGenerateRoute: (settings) {
@@ -111,20 +111,15 @@ class Root extends StatefulWidget {
 class _RootState extends State<Root> {
   @override
   Widget build(BuildContext context) {
-    const double margin = 8;
-
     reset() {
       setState(() {});
     }
 
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // title: Text(widget.title),
-
+        backgroundColor: Colors.grey.shade200,
         toolbarHeight: 40,
         elevation: 0,
-        backgroundColor: Colors.transparent,
         leading: IconButton(
           hoverColor: Colors.transparent,
           onPressed: () {},
@@ -182,11 +177,11 @@ class _RootState extends State<Root> {
                     );
                   }
 
+                  final user = User.fromMap(data.data!.first);
+
                   return Container(
-                    color: const Color.fromARGB(255, 100, 64, 141),
                     constraints: const BoxConstraints.expand(),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    child: ListView(
                       children: [
                         // const Text(
                         //   "Sample Landing Page",
@@ -237,36 +232,36 @@ class _RootState extends State<Root> {
 
                         Text(
                           'DiaLife',
+                          textAlign: TextAlign.center,
                           style: GoogleFonts.italianno(
                             fontSize: 59,
                             color: const Color.fromRGBO(76, 102, 240, 1.0),
                           ),
                         ),
                         // Current health status text label
-                        Center(
-                          child: SizedBox(
-                            height: 23,
-                            width: 198,
-                            child: Text(
-                              'Current Health Status',
-                              style: GoogleFonts.inter(
-                                fontSize: 19,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
+                        Text(
+                          'Current Health Status',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.inter(
+                            fontSize: 19,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
 
                         // Container for health progress bar
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 8.0,
-                            left: 30.0,
-                            right: 30.0,
-                          ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context)
+                                .pushNamed("/blood-glucose-tracking");
+                          },
                           child: Container(
                             width: double.infinity,
                             height: 280,
+                            margin: const EdgeInsets.only(
+                              left: 10,
+                              right: 10,
+                              top: 10,
+                            ),
                             decoration: BoxDecoration(
                               color: Colors.white,
                               borderRadius: BorderRadius.circular(10.0),
@@ -280,64 +275,84 @@ class _RootState extends State<Root> {
                               ],
                             ),
                             child: Column(
-                              children: [
-                                // GestureDetector(
-                                //   onTap: () {
-                                //     Navigator.pushNamed(context, '/ui');
-                                //   },
-                                //   child: const Padding(
-                                //     padding: EdgeInsets.only(
-                                //       top: 39.0,
-                                //       left: 31.0,
-                                //       right: 31.0,
-                                //     ),
-                                //     child: GlucoseStatusBar(),
-                                //   ),
-                                // ),
-                              ],
+                              children: [],
                             ),
                           ),
                         ),
 
                         // Currently empty container
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 18.0,
-                            left: 30.0,
-                            right: 30.0,
+                        Container(
+                          margin: const EdgeInsets.only(
+                            right: 10,
+                            left: 10,
+                            top: 15,
                           ),
-                          child: Container(
-                            width: double.infinity,
-                            height: 68,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10.0),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.25),
-                                  blurRadius: 4.0,
-                                  spreadRadius: 0.0,
-                                  offset: const Offset(0.0, 4.0),
-                                ),
-                              ],
-                            ),
+                          width: double.infinity,
+                          height: 68,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.25),
+                                blurRadius: 4.0,
+                                spreadRadius: 0.0,
+                                offset: const Offset(0.0, 4.0),
+                              ),
+                            ],
                           ),
                         ),
 
                         // Empty containers
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 18.0,
-                            left: 30.0,
-                            right: 30.0,
+                        Container(
+                          margin: const EdgeInsets.only(
+                            right: 10,
+                            left: 10,
+                            top: 15,
                           ),
-                          child: SizedBox(
-                            height: 224,
-                            child: Row(
-                              children: [
-                                Column(
-                                  children: [
-                                    Container(
+                          height: 224,
+                          child: Row(
+                            children: [
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  // NOTE: Medication
+                                  Container(
+                                    width: 114,
+                                    height: 104,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.25),
+                                          blurRadius: 4,
+                                          spreadRadius: 0,
+                                          offset: const Offset(0, 4),
+                                        )
+                                      ],
+                                    ),
+                                    child: Center(
+                                      child: IconButton(
+                                        icon: const Icon(
+                                          Icons.notifications,
+                                          size: 35,
+                                        ),
+                                        onPressed: () {
+                                          // Navigator.pushNamed(
+                                          //     context, '/notifPage');
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  // NOTE: BMI
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.of(context)
+                                          .pushNamed("/bmi-tracking");
+                                    },
+                                    child: Container(
                                       width: 114,
                                       height: 104,
                                       decoration: BoxDecoration(
@@ -354,88 +369,50 @@ class _RootState extends State<Root> {
                                           )
                                         ],
                                       ),
-                                      child: Center(
-                                        child: IconButton(
-                                          icon: const Icon(
-                                            Icons.notifications,
-                                            size: 35,
-                                          ),
-                                          onPressed: () {
-                                            // Navigator.pushNamed(
-                                            //     context, '/notifPage');
-                                          },
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 16.0),
-                                      child: Container(
-                                        width: 114,
-                                        height: 104,
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius:
-                                              BorderRadius.circular(10.0),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: Colors.black
-                                                  .withOpacity(0.25),
-                                              blurRadius: 4,
-                                              spreadRadius: 0,
-                                              offset: const Offset(0, 4),
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  width: 11.0,
-                                ),
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.black.withOpacity(0.25),
-                                          blurRadius: 4,
-                                          spreadRadius: 0,
-                                          offset: const Offset(0, 4),
-                                        )
-                                      ],
                                     ),
                                   ),
+                                ],
+                              ),
+                              const SizedBox(width: 15),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.25),
+                                        blurRadius: 4,
+                                        spreadRadius: 0,
+                                        offset: const Offset(0, 4),
+                                      )
+                                    ],
+                                  ),
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
                         ),
 
                         // empty container
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 16.0,
-                            left: 30.0,
-                            right: 30.0,
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 15,
                           ),
-                          child: Container(
-                            width: double.infinity,
-                            height: 68,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10.0),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.25),
-                                  blurRadius: 4.0,
-                                  spreadRadius: 0.0,
-                                  offset: const Offset(0.0, 4.0),
-                                ),
-                              ],
-                            ),
+                          width: double.infinity,
+                          height: 68,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.25),
+                                blurRadius: 4.0,
+                                spreadRadius: 0.0,
+                                offset: const Offset(0.0, 4.0),
+                              ),
+                            ],
                           ),
                         ),
                       ],
