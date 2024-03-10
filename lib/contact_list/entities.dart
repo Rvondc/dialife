638315926959e86b02e9604510279706f9e58156
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-03-10 17:17:47 +0800

    added field contact number

--- lib/contact_list/entities.dart
+++ lib/contact_list/entities.dart
@@ -1,18 +1,19 @@
 class Doctor {
   final int id;
-  final String facebookId;
+  final String? facebookId;
   final String name;
   final String address;
   final String description;
+  final String? phoneNumber;
 
   static Doctor fromMap(Map<String, dynamic> map) {
     return Doctor(
-      id: map["id"],
-      facebookId: map["facebook_id"],
-      name: map["name"],
-      address: map["address"],
-      description: map["description"],
-    );
+        id: map["id"],
+        facebookId: map["facebook_id"],
+        name: map["name"],
+        address: map["address"],
+        description: map["description"],
+        phoneNumber: map["contact_number"]);
   }
 
   static List<Doctor> fromListOfMaps(List<Map<String, dynamic>> list) {
@@ -27,5 +28,6 @@ class Doctor {
     required this.name,
     required this.address,
     required this.description,
+    required this.phoneNumber,
   });
 }
